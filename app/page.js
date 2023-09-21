3d4dbaf7fa2815b1ec4cb66444a8ffd580454271
Joy Debnath <96875662+developerjoydebnath@users.noreply.github.com>
2023-09-21 23:33:09 +0600

    Update page.js
    change

--- app/page.js
+++ app/page.js
@@ -2,9 +2,6 @@ export default function Home() {
     return (
         <main>
             <h1>hello </h1>
-            <h1>hello </h1>
-            <h1>hello </h1>
-            <h1>hello </h1>
         </main>
     );
 }
